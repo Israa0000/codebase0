1a0b6fac3f444dfd6d79489f511c4691cf6d349e
israa0000 <eiorrree90@gamil.com>
2025-03-24 12:53:03 +0100

    backend-parking

---
+++ 3trim/bbdd/Parking-3-piso/better.js
@@ -0,0 +1,11 @@
+const squlite3 = require('better-sqlite3');
+
+const db = new squlite3('database.db')
+
+const respuesta = db.prepare('SELECT * FROM plazas').all();
+
+console.log(respuesta)
+
+//hola mundo con express( conseguri servidor de express que devuelva un hola mundo)
+// endpoint /plazas que devuelva el resltado de  
+// const respuesta = db.prepare('SELECT * FROM plazas').all();
